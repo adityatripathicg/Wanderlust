481e1fac148dcda1173e971a574ac889e7ff3e21
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-29 03:21:00 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use((req,res,next)=>{ // Success Error Flash
     next();
 });
 
-
+app.use("/", listingsRouter);
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", usersRouter);
